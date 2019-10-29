e21a8108b756ce05699ce16bef3e522b6de4f9ba
Tiera Salmi <tiera.salmi@gmail.com>
2019-10-29 20:45:37 +0200

    testikattavuus 100%

--- src/test/java/ohtu/ohtuvarasto/VarastoTest.java
+++ src/test/java/ohtu/ohtuvarasto/VarastoTest.java
@@ -13,6 +13,7 @@ import static org.junit.Assert.*;
 public class VarastoTest {
 
     Varasto varasto;
+
     double vertailuTarkkuus = 0.0001;
 
     @Before
@@ -31,11 +32,55 @@ public class VarastoTest {
     }
 
     @Test
+    public void viallinenVarastoOnKelvoton() {
+        varasto = new Varasto(-30000);
+        assertEquals(0, varasto.getTilavuus(), vertailuTarkkuus);
+    }
+
+    @Test
+    public void alkusaldollinenJaViallinenVarastoOnKelvoton() {
+        varasto = new Varasto(-30000, 3);
+        assertEquals(0, varasto.getTilavuus(), vertailuTarkkuus);
+    }
+
+    @Test
+    public void alkusaldoAsettuuOikein() {
+        varasto = new Varasto(30, 10);
+        assertEquals(10, varasto.getSaldo(), vertailuTarkkuus);
+    }
+
+    @Test
+    public void ylivuotavaAlkusaldoAsettuuOikein() {
+        varasto = new Varasto(30, 3000);
+        assertEquals(30, varasto.getSaldo(), vertailuTarkkuus);
+    }
+
+    @Test
+    public void negatiivinenAlkusaldoAsettuuOikein() {
+        varasto = new Varasto(30, -3000);
+        assertEquals(0, varasto.getSaldo(), vertailuTarkkuus);
+    }
+
+    @Test
     public void lisaysLisaaSaldoa() {
         varasto.lisaaVarastoon(8);
 
         // saldon pitäisi olla sama kun lisätty määrä
         assertEquals(8, varasto.getSaldo(), vertailuTarkkuus);
+    }
+
+    @Test
+    public void virheellinenLisaysEiLisaaSaldoa() {
+        varasto.lisaaVarastoon(-3);
+
+        assertEquals(0, varasto.getSaldo(), vertailuTarkkuus);
+    }
+
+    @Test
+    public void ylivuotavaLisaysMaksimoiSaldon() {
+        varasto.lisaaVarastoon(varasto.getTilavuus() + 1);
+
+        assertEquals(varasto.getTilavuus(), varasto.getSaldo(), vertailuTarkkuus);
     }
 
     @Test
@@ -56,13 +101,40 @@ public class VarastoTest {
     }
 
     @Test
-    public void ottaminenLisääTilaa() {
+    public void ottaminenLisaaTilaa() {
         varasto.lisaaVarastoon(8);
 
         varasto.otaVarastosta(2);
 
         // varastossa pitäisi olla tilaa 10 - 8 + 2 eli 4
         assertEquals(4, varasto.paljonkoMahtuu(), vertailuTarkkuus);
+    }
+
+    @Test
+    public void virheellinenOttaminenPalauttaaNollan() {
+        varasto.lisaaVarastoon(8);
+
+        double saatuMaara = varasto.otaVarastosta(-3);
+
+        assertEquals(0, saatuMaara, vertailuTarkkuus);
+    }
+
+    @Test
+    public void ylivuotavaOttaminenTyhjentaa() {
+        varasto.lisaaVarastoon(8);
+
+        double saatuMaara = varasto.otaVarastosta(8000);
+
+        assertEquals(8, saatuMaara, vertailuTarkkuus);
+        assertEquals(0, varasto.getSaldo(), vertailuTarkkuus);
+    }
+
+    @Test
+    public void merkkijonoesitysToimiiOikein() {
+        varasto.lisaaVarastoon(10);
+        String toString = "saldo = 10.0, vielä tilaa 0.0";
+
+        assertEquals(toString, varasto.toString());
     }
 
 }
