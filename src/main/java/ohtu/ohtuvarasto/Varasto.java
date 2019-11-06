cba29d662bc4e89432076a517b5224fadf3e1767
Tiera Salmi <tiera.salmi@gmail.com>
2019-11-06 18:00:48 +0200

    tehtävä 2 tehty

--- src/main/java/ohtu/ohtuvarasto/Varasto.java
+++ src/main/java/ohtu/ohtuvarasto/Varasto.java
@@ -3,35 +3,17 @@ package ohtu.ohtuvarasto;
 public class Varasto {
 
     // --- piilotettu tietorakenteen toteutus: ---
-    private double tilavuus;  // paljonko varastoon mahtuu,  > 0
-    private double saldo;     // paljonko varastossa on nyt, >= 0
+    private double tilavuus; // paljonko varastoon mahtuu, > 0
+    private double saldo; // paljonko varastossa on nyt, >= 0
 
     // --- konstruktorit: ---
-    public Varasto(double tilavuus) {  // tilavuus on annettava
-        if (tilavuus > 0.0) {
-            this.tilavuus = tilavuus;
-        } else // virheellinen, nollataan
-        {
-            this.tilavuus = 0.0;  // => käyttökelvoton varasto
-        }
-        saldo = 0;     // oletus: varasto on tyhjä
+    public Varasto(double tilavuus) { // tilavuus on annettava
+        this.setTilavuus(tilavuus);
     }
 
     public Varasto(double tilavuus, double alkuSaldo) { // kuormitetaan
-        if (tilavuus > 0.0) {
-            this.tilavuus = tilavuus;
-        } else // virheellinen, nollataan
-        {
-            this.tilavuus = 0.0;  // => käyttökelvoton varasto
-        }
-        if (alkuSaldo < 0.0) {
-            this.saldo = 0.0;
-        } else if (alkuSaldo <= tilavuus) // mahtuu
-        {
-            this.saldo = alkuSaldo;
-        } else {
-            this.saldo = tilavuus;  // täyteen ja ylimäärä hukkaan!
-        }
+        this.setTilavuus(tilavuus);
+        this.setSaldo(alkuSaldo);
     }
 
     // --- ottavat aksessorit eli getterit: ---
@@ -39,40 +21,57 @@ public class Varasto {
         return saldo;
     }
 
+    public void setTilavuus(double tilavuus) {
+        if (tilavuus > 0) {
+            this.tilavuus = tilavuus;
+        } else {
+            this.tilavuus = 0;
+        }
+    }
+
+    public void setSaldo(double alkuSaldo) {
+        this.saldo = 0;
+        if (alkuSaldo < 0) {
+            return;
+        }
+        if (alkuSaldo < this.tilavuus) {
+            this.saldo = alkuSaldo;
+        } else {
+            this.saldo = this.tilavuus;
+        }
+    }
+
     public double getTilavuus() {
         return tilavuus;
     }
 
-    public double paljonkoMahtuu() {  // huom: ominaisuus voidaan myös laskea
-        return tilavuus - saldo;        //  ei tarvita erillistä kenttää vielaTilaa tms.
+    public double paljonkoMahtuu() { // huom: ominaisuus voidaan myös laskea
+        return tilavuus - saldo; // ei tarvita erillistä kenttää vielaTilaa tms.
     }
 
     // --- asettavat aksessorit eli setterit: ---
     public void lisaaVarastoon(double maara) {
-        if (maara < 0) // virhetilanteessa voidaan tehdä 
-        {
-            return;       // tällainen pikapoistuminenkin!
+        if (maara < 0) {
+            return; // tällainen pikapoistuminenkin!
         }
-        if (maara <= paljonkoMahtuu()) // omia aksessoreita voi kutsua
-        {
-            saldo = saldo + maara;          // ihan suoraan sellaisinaan
+        if (maara <= paljonkoMahtuu()) {
+            saldo = saldo + maara; // ihan suoraan sellaisinaan
         } else {
-            saldo = tilavuus;  // täyteen ja ylimäärä hukkaan!
+            saldo = tilavuus; // täyteen ja ylimäärä hukkaan!
         }
     }
 
     public double otaVarastosta(double maara) {
-        if (maara < 0) // virhetilanteessa voidaan tehdä 
-        {
-            return 0.0;   // tällainen pikapoistuminenkin!
+        if (maara < 0) {
+            return 0.0; // tällainen pikapoistuminenkin!
         }
-        if (maara > saldo) {          // annetaan mitä voidaan
+        if (maara > saldo) { // annetaan mitä voidaan
             double kaikkiMitaVoidaan = saldo;
-            saldo = 0.0;               // ja tyhjäksi menee
-            return kaikkiMitaVoidaan;  // poistutaan saman tien
+            saldo = 0.0; // ja tyhjäksi menee
+            return kaikkiMitaVoidaan; // poistutaan saman tien
         }
         // jos tänne päästään, kaikki pyydetty voidaan antaa
-        saldo = saldo - maara;  // vähennetään annettava saldosta
+        saldo = saldo - maara; // vähennetään annettava saldosta
         return maara;
     }
 
--- src/test/java/ohtu/ohtuvarasto/VarastoTest.java
+++ src/test/java/ohtu/ohtuvarasto/VarastoTest.java
@@ -52,6 +52,7 @@ public class VarastoTest {
     @Test
     public void ylivuotavaAlkusaldoAsettuuOikein() {
         varasto = new Varasto(30, 3000);
+        System.out.println("saldo testissä :" + varasto.getSaldo());
         assertEquals(30, varasto.getSaldo(), vertailuTarkkuus);
     }
 
